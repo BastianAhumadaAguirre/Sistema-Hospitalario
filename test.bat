66803419918ce273c917d3c1c16aceb82aeeab39
Benjamin Marfull <benja.marfull@gmail.com>
2026-06-02 20:57:31 -0400

    chore: agrega script de ejecucion de pruebas
    - test.bat ejecuta los 3 microservicios en secuencia
    - Abre automaticamente el reporte JaCoCo en el navegador al finalizar

---
+++ test.bat
@@ -0,0 +1,33 @@
+@echo off
+echo.
+echo ========================================
+echo  Ejecutando pruebas: lista-espera-service
+echo ========================================
+cd lista-espera-service
+call mvnw.cmd test
+start "" "%CD%\target\site\jacoco\index.html"
+cd ..
+
+echo.
+echo ========================================
+echo  Ejecutando pruebas: paciente-service
+echo ========================================
+cd paciente-service
+call mvnw.cmd test
+start "" "%CD%\target\site\jacoco\index.html"
+cd ..
+
+echo.
+echo ========================================
+echo  Ejecutando pruebas: medico-service
+echo ========================================
+cd medico-service
+call mvnw.cmd test
+start "" "%CD%\target\site\jacoco\index.html"
+cd ..
+
+echo.
+echo ========================================
+echo  Pruebas finalizadas - reportes abiertos
+echo ========================================
+pause
